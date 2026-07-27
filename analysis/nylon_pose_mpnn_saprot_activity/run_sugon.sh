6cd0ae4229cce5ea7bbd35562ab364435edc8b0d
bianhuiting918 <93578622+bianhuiting918@users.noreply.github.com>
2026-07-27 22:19:01 +0800

    ops: use latest audited exact SaProt paired manifest

--- analysis/nylon_pose_mpnn_saprot_activity/run_sugon.sh
+++ analysis/nylon_pose_mpnn_saprot_activity/run_sugon.sh
@@ -16,7 +16,7 @@ set +e
  --pose-strata "$ROOT/inputs/pose_enzyme_retention_by_stratum.tsv" \
  --pose-not-evaluated "$ROOT/inputs/pose_not_evaluated.tsv" \
  --mpnn /work/home/acshdt1dks/petase_orbmol_lg1_lg4_layer8343_20260721/outputs/proteinmpnn_compatibility_pet8329_nylon4167_20260727_v2_final/proteinmpnn_candidate_scores.tsv \
- --saprot /work/home/acshdt1dks/petase_orbmol_lg1_lg4_layer8343_20260721/outputs/saprot_nylonase_4556_denominator_20260725_v1/nylonase_4556_saprot_denominator.tsv \
+ --saprot /work/home/acshdt1dks/petase_orbmol_lg1_lg4_layer8343_20260721/manifests/proteinmpnn_compatibility_pet8329_nylon4167_20260727_v2_scopefix/run_manifest_all_12496_scopefixed.tsv \
  --output "$ROOT" --n-boot 10000 --seed 20260727
 rc=$?
 if [[ $rc -eq 0 ]]; then
